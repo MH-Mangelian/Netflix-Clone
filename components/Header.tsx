32486cf54996527be0e9bf074662b5e4e48917f5
Mohammad Hossein Mangelian <h.m.mnglean@gmail.com>
2023-03-11 15:38:28 +0330

    make Scroll Header bg-auto

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,22 @@
 import Link from "next/link";
-import {HiSearch , HiBell,HiUser } from "react-icons/hi";
-
+import { HiSearch, HiBell, HiUser } from "react-icons/hi";
+import { useState , useEffect} from "react";
 const Header = () => {
+  const [isScroll, setIsScroll] = useState(false);
+  useEffect(() => {
+    const handleScroll = () => {
+        if (window.scrollY > 0) {
+            setIsScroll(true);
+        } else {
+            setIsScroll(false);
+    }   
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }}, []);
+
   return (
-    <header>
+    <header className={`${ isScroll && 'bg-[hsl(0,0%,8%)]'}`}>
       {/* --------------------------Left---------------------------------- */}
       <div className="flex items-center space-x-2 md:space-x-10">
         <img
@@ -15,28 +28,25 @@ const Header = () => {
         />
 
         <ul className="hidden space-x-4 md:flex">
-            <li className="headerLink">Home</li>
-            <li className="headerLink">TV Shows</li>
-            <li className="headerLink">Movies</li>
-            <li className="headerLink">New & Popular</li>
-            <li className="headerLink">My List</li>
+          <li className="headerLink">Home</li>
+          <li className="headerLink">TV Shows</li>
+          <li className="headerLink">Movies</li>
+          <li className="headerLink">New & Popular</li>
+          <li className="headerLink">My List</li>
         </ul>
-
       </div>
       {/* --------------------------End Left---------------------------------- */}
 
-
       {/* ---------------------------Right------------------------------- */}
       <div className="flex items-center space-x-4 text-sm font-light">
-        <HiSearch className="hidden h-6 w-6 cursor-pointer sm:inline "/>
+        <HiSearch className="hidden h-6 w-6 cursor-pointer sm:inline " />
         <p className="hidden lg:inline">Kids</p>
-        <HiBell className="h-6 w-6 cursor-pointer "/>
+        <HiBell className="h-6 w-6 cursor-pointer " />
         <Link href="/account">
-            <HiUser className="h-6 w-6 cursor-pointer"/>
+          <HiUser className="h-6 w-6 cursor-pointer" />
         </Link>
       </div>
       {/* ---------------------------End Right------------------------------- */}
-
     </header>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,14 @@
 import Head from 'next/head'
 import { Inter } from 'next/font/google'
-import Header from '@/components/Header'
+import Header  from '@/components/Header'
+import  Banner  from '@/components/Banner'
 
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <div className=''>
+    <div className='relative h-screen bg-gradient-to-b from-slate-900/10 to-[hsl(225,85%,5%)] lg:h-[140vh]'>
       <Head>
         <title>NetFlix - Home</title>
         <link rel="icon" href="/favicon.ico" />
@@ -15,7 +16,7 @@ export default function Home() {
       
       <Header/>
       <main>
-        {/* {------------------------------Banner----------------------------------} */}
+        <Banner/>
         <section>
           {/* {--------------row------------------} */}
           {/* {--------------row------------------} */}
